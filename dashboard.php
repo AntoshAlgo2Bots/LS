23159b4c2021c34042997d696cd84b14d343684e
AntoshAlgo2Bots <antosh.pandey@algo2botsinfotech.com>
2024-11-30 13:06:32 +0530

    new

--- dashboard.php
+++ dashboard.php
@@ -1901,7 +1901,7 @@ $role = $_SESSION['role'] ?? ''; // Ensure role is set
                                             Serial report
                                         </a>
 
-                                        <a href="./serialHereakchy.php"
+                                        <a href="./moveOrderReport.php"
                                             class="flex items-center w-full p-3 leading-tight transition-all rounded-lg outline-none text-start hover:bg-blue-gray-50 hover:bg-opacity-80 hover:text-blue-gray-900 focus:bg-blue-gray-50 focus:bg-opacity-80 focus:text-blue-gray-900 active:bg-blue-gray-50 active:bg-opacity-80 active:text-blue-gray-900">
                                             <div class="grid mr-4 place-items-center">
                                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
